c7c756db41ba20246f003add8c745d3e1cf06c30
David Parlevliet <dave@leaguecraft.com>
2012-11-02 15:11:01 +1100

    Use Lipsum to prove it can do larger, odd sized blocks.

--- test.js
+++ test.js
@@ -2,7 +2,9 @@
 var Encryption = require('./encryption.js');
 var e = new Encryption();
 var key = "test_key";
-var encrypted = e.encrypt("password", key);
+var text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris fringilla, risus id varius porttitor, metus ligula consectetur mi, id vulputate sapien tortor sit amet nunc.";
+
+var encrypted = e.encrypt(text, key);
 console.log("Encrypted text: "+encrypted);
 
 var decrypted = e.decrypt(encrypted, key);
