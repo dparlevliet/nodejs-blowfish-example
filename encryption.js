9cf1bdf4ff374a866ba643581b356bd9e2676d45
Jefrey Sobreira Santos <jesobreira@yahoo.com.br>
2016-11-27 03:40:41 -0200

    fixed deprecated warning
    DeprecationWarning: Using Buffer without `new` will soon stop working. Use `new Buffer()`, or preferably `Buffer.from()`, `Buffer.allocUnsafe()` or `Buffer.alloc()` instead.

--- encryption.js
+++ encryption.js
@@ -1,4 +1,3 @@
-
 var crypto = require('crypto');
 var algorithm = "bf-ecb";
 
@@ -12,20 +11,20 @@ function pad(text) {
 function Encryption() {
   self = this;
   self.encrypt = function(data, key) {
-    var cipher = crypto.createCipheriv(algorithm, Buffer(key), '');
+    var cipher = crypto.createCipheriv(algorithm, new Buffer(key), '');
     cipher.setAutoPadding(false);
     try {
-      return Buffer(cipher.update(pad(data), 'utf8', 'binary') + cipher.final('binary'), 'binary').toString('base64');
+      return new Buffer(cipher.update(pad(data), 'utf8', 'binary') + cipher.final('binary'), 'binary').toString('base64');
     } catch (e) {
       return null;
     }
   }
 
   self.decrypt = function(data, key) {
-    var decipher = crypto.createDecipheriv(algorithm, Buffer(key), '');
+    var decipher = crypto.createDecipheriv(algorithm, new Buffer(key), '');
     decipher.setAutoPadding(false);
     try {
-      return (decipher.update(Buffer(data, 'base64').toString('binary'), 'binary', 'utf8')+ decipher.final('utf8')).replace(/\x00+$/g, '');
+      return (decipher.update(new Buffer(data, 'base64').toString('binary'), 'binary', 'utf8')+ decipher.final('utf8')).replace(/\x00+$/g, '');
     } catch (e) {
       return null;
     }
